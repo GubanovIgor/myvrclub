cf826163c01d33134e200e4a49d05498d5b29296
Gubanov Igor <applecake@inbox.ru>
2019-08-22 11:19:34 +0300

    инициализировал приложение next.js

---
+++ frontend/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Link href='/'>
+          Home
+        </Link>
+        <Link href='/games'>
+          Games
+        </Link>
+          INDEX
+  </div>
+    )
+  }
+}
+
+export default Header;
---
+++ frontend/pages/Games.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+const Games = () => (
+  <div>
+    <Head>
+    </Head>
+
+    <Link href='/'>
+      Home
+    </Link>
+    <Link href='/games'>
+      Games
+    </Link>
+
+    
+    GAMES
+  </div>
+)
+
+export default Games
