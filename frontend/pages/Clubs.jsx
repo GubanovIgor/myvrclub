27bd8f7ee989daf490ee918fa4da9be40ed0cbc8
Gubanov Igor <applecake@inbox.ru>
2019-08-22 11:36:33 +0300

    сделали навигацию

---
+++ frontend/pages/Clubs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const Clubs = () => (
+  <div>
+    <Navbar />
+  </div>
+)
+
+export default Clubs
---
+++ frontend/pages/Games.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const Games = () => (
+  <div>
+    <Navbar />
+  </div>
+)
+
+export default Games
