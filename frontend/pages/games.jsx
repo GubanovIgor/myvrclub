087a2a9dc1aaad231eb43077e897e37599883ab5
Gubanov Igor <applecake@inbox.ru>
2019-08-25 13:51:11 +0300

    сделал форму поиска на главной странице

--- frontend/pages/games.jsx
+++ frontend/pages/games.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// import components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import GameCard from '../components/GameCard';
