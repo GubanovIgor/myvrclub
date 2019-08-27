e76271c07c95a7dda086abde1d7a5b76bb5e7a1e
Gubanov Igor <applecake@inbox.ru>
2019-08-27 16:16:18 +0300

    не могу сделать фильтр монго

--- backend/routes/club.js
+++ backend/routes/club.js
@@ -8,15 +8,26 @@ const Club = require('../models/clubs');
 // });
 
 router.post('/', async (req, res) => {
-  console.log(req.body.checkedToggle)
-  if (!req.body.checkedToggle.length) {
+
+  let num = 0;
+
+  if (req.body.checkedToggle[1].length) {
+    const length = req.body.checkedToggle[1].length;
+    num = req.body.checkedToggle[1][length-1].split(' ');
+    num = parseInt(num[1])
+    // console.log(num);
+  }
+
+  if (!req.body.checkedToggle[0].length && !req.body.checkedToggle[1].length) {
     const clubs = await Club.find();
     res.json(clubs);
   } else {
-    const arr = [];
-    const club = await Club.findOne();
-    arr.push(club)
-    res.json(arr);
+    console.log(num)
+    const clubs = await Club.find(
+      { $or: [{ equipment: { $all: req.body.checkedToggle[0] }}, {price: { $lt: num } }] }
+      )
+    // const clubs = await Club.find();
+    res.json(clubs);
   }
 });
 
--- frontend/redux/actions.js
+++ frontend/redux/actions.js
@@ -10,14 +10,14 @@ export const getClubsAC = (filterToggleData) => (
   async (dispatch) => {
 
     // Оставляем в массиве checkedToggle только те тоглы, у которых значение true
-    let checkedToggle = [];
+    let checkedToggle = [[], []];
     if(filterToggleData) {
       const keys = Object.keys(filterToggleData);
       for (let i = 0; i < keys.length; i++) {
         const categoryKeys = Object.keys(filterToggleData[keys[i]]);
         categoryKeys.forEach((key) => {
           if (filterToggleData[keys[i]][key]) {
-            checkedToggle.push(key)
+            checkedToggle[i].push(key)
           }
         })
       }
