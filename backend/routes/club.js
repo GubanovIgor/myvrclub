942953943d8a3bae719fae214ee2eef80f202f59
Gubanov Igor <applecake@inbox.ru>
2019-08-28 09:39:36 +0300

    сделали фильтр клубов

--- backend/routes/club.js
+++ backend/routes/club.js
@@ -9,26 +9,24 @@ const Club = require('../models/clubs');
 
 router.post('/', async (req, res) => {
 
-  let num = 0;
+  const conditions =[];
+
+  if (req.body.checkedToggle[0].length) {
+    conditions.push({ equipment: { $all: req.body.checkedToggle[0] }})
+  }
 
   if (req.body.checkedToggle[1].length) {
     const length = req.body.checkedToggle[1].length;
-    num = req.body.checkedToggle[1][length-1].split(' ');
-    num = parseInt(num[1])
-    // console.log(num);
+    const num = req.body.checkedToggle[1][length-1].split(' ')[1];
+    console.log(num);
+    conditions.push({price: { $lt: num } })
   }
 
-  if (!req.body.checkedToggle[0].length && !req.body.checkedToggle[1].length) {
-    const clubs = await Club.find();
-    res.json(clubs);
-  } else {
-    console.log(num)
-    const clubs = await Club.find(
-      { $or: [{ equipment: { $all: req.body.checkedToggle[0] }}, {price: { $lt: num } }] }
-      )
-    // const clubs = await Club.find();
-    res.json(clubs);
-  }
+  const clubs = await Club.find(
+    conditions.length ? { $and: conditions } : {}
+  );
+
+  res.json(clubs);
 });
 
 module.exports = router;
