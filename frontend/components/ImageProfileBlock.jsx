9f5678b6043576959090e3eabceb7cd6e3cfb6aa
Roman <filpoyma@gmail.com>
2019-08-30 14:36:33 +0300

    переключение картинок

--- frontend/components/ImageProfileBlock.jsx
+++ frontend/components/ImageProfileBlock.jsx
@@ -13,7 +13,7 @@ class ImageProfileBlock extends Component {
   };
 
   componentDidMount() {
-  const { club = [], game = [], isClub, isGame } = this.props;
+    const { club = [], game = [], isClub, isGame } = this.props;
     if (isClub) this.setState({ imageUrl: IMG_URL_PREFIX + club.screenShot[0] });
     if (isGame) this.setState({ imageUrl: game.screenShot[0] });
   }
@@ -23,13 +23,14 @@ class ImageProfileBlock extends Component {
     this.setState({ imageUrl });
   };
 
-  render(){
+  render() {
     const { club = [], game = [], isClub, isGame } = this.props;
     if (isClub) return (
       <div>
         {console.log('this.state.imageUrl', this.state.imageUrl)}
         {/*<Image cloudName="myvrclub" publicId="sample" width="300" crop="scale"/>*/}
-        <img className={styles.img} key={uuidv1()} src={IMG_URL_PREFIX + club.screenShot[0]} width="750" height="421"
+        <img className={styles.img} key={uuidv1()} src={this.state.imageUrl} width="750"
+             height="421"
              alt={club.name}/>
         <div className={styles.screenshotsSwitch}>
           {club.screenShot.map((imgLink, index) => {
@@ -47,13 +48,17 @@ class ImageProfileBlock extends Component {
     if (isGame) return (
       <div>
         {/*<Image cloudName="myvrclub" publicId="sample" width="300" crop="scale"/>*/}
-        <img className={styles.img} key={uuidv1()} src={game.screenShot[0]} width="750" height="421"
+        <img className={styles.img} key={uuidv1()} src={this.state.imageUrl} width="750"
+             height="421"
              alt={game.name}/>
         <div className={styles.screenshotsSwitch}>
           {game.screenShot.map((imgLink, index) => {
             if (index > 4) return;
-            return <img className={styles.screen} key={uuidv1()} src={imgLink} width="144" height="81"
-                        alt={game.name}/>
+            return <img className={styles.screen}
+                        key={uuidv1()} src={imgLink}
+                        width="144" height="81"
+                        alt={game.name}
+                        onClick={() => this.updateUrlImage(imgLink)}/>
           })}
         </div>
       </div>
@@ -61,38 +66,30 @@ class ImageProfileBlock extends Component {
   }
 }
 
-// const ImageProfileBlock = (props) => {
-//   const { club = [], game = [], isClub, isGame } = props;
-//   if (isClub) return (
-//       <div>
-//         {/*<Image cloudName="myvrclub" publicId="sample" width="300" crop="scale"/>*/}
-//         <img className={styles.img} key={uuidv1()} src={IMG_URL_PREFIX + club.screenShot[0]} width="750" height="421"
-//              alt={props.club.name}/>
-//         <div className={styles.screenshotsSwitch}>
-//           {club.screenShot.map((imgLink, index) => {
-//             if (index === 0) return;
-//             return <img className={styles.screen} key={uuidv1()} src={IMG_URL_PREFIX + imgLink} width="144" height="81"
-//                         alt={props.club.name}/>
-//           })}
-//         </div>
-//       </div>
-//     );
 //
-//   if (isGame) return (
-//     <div>
-//       {/*<Image cloudName="myvrclub" publicId="sample" width="300" crop="scale"/>*/}
-//       <img className={styles.img} key={uuidv1()} src={game.screenShot[0]} width="750" height="421"
-//            alt={props.game.name}/>
-//       <div className={styles.screenshotsSwitch}>
-//         {game.screenShot.map((imgLink, index) => {
-//           if (index === 0 || index > 5) return;
-//           return <img className={styles.screen} key={uuidv1()} src={imgLink} width="144" height="81"
-//                       alt={props.game.name}/>
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
 
 
 export default ImageProfileBlock;
+
+
+// if (isGame) let par = 'game';
+// if (isClub) let par = 'club';
+//
+//   return (
+//   <div>
+//     {/*<Image cloudName="myvrclub" publicId="sample" width="300" crop="scale"/>*/}
+//     <img className={styles.img} key={uuidv1()} src={this.state.imageUrl} width="750"
+//          height="421"
+//          alt={[par].name}/>
+//     <div className={styles.screenshotsSwitch}>
+//       {[par].screenShot.map((imgLink, index) => {
+//         if (index > 4) return;
+//         return <img className={styles.screen}
+//                     key={uuidv1()} src={imgLink}
+//                     width="144" height="81"
+//                     alt={[par].name}
+//                     onClick={() => this.updateUrlImage(imgLink)}/>
+//       })}
+//     </div>
+//   </div>
+// )
