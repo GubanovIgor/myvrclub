13a633ab420dd41e47a6fdc807c8b4ce8920f20f
Игорь <applecake@inbox.ru>
2019-09-27 13:50:37 +0300

    выровнял иконку рейтинга на играх

--- frontend/components/GameCard.jsx
+++ frontend/components/GameCard.jsx
@@ -6,11 +6,11 @@ import styles from '../stylesheets/gameCard.module.scss';
 
 class GameCard extends Component {
   render() {
-    const {game = []} = this.props;
+    const { game = [] } = this.props;
     return (
-        <Link href='/games/[urlname]' as={`/games/${game.urlName}`}>
+      <Link href='/games/[urlname]' as={`/games/${game.urlName}`}>
         <div className={styles.container}>
-          {<img className={styles.cover} src={game.cover}/>}
+          {<img className={styles.cover} src={game.cover} />}
           <p className={styles.title}>{game.name}</p>
           {/*<div className={styles.new}>new</div>*/}
           <div className={styles.rating}>{Math.floor(Math.random() * 10) + 1}</div>
