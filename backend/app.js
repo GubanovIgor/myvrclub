eff60ca0c1df79a80308f3b94dfb06763b69c0cb
Roman <filpoyma@gmail.com>
2019-10-06 15:20:15 +0300

    remote db

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,9 @@ const adminRouter = require('./routes/admin');
 const port = process.env.PORT || 3100;
 let app = express();
 
-const dbName = 'mongodb://localhost/myvrclub';
+//const dbName = 'mongodb://localhost/myvrclub';
 //const dbName = 'mongodb+srv://mongo:12345@cluster0-xe8h0.mongodb.net/test?retryWrites=true&w=majority';
-// const dbName = `mongodb+srv://rom:${process.env.PASSW_DB}@cluster0-woi64.mongodb.net/myvrclub`;
+const dbName = `mongodb+srv://rom:${process.env.PASSW_DB}@cluster0-woi64.mongodb.net/myvrclub`;
 mongoose.connect(dbName, { useNewUrlParser: true, useCreateIndex: true });
 
 app.use(morgan('dev'));
